e09fca4bdcbb2e4a295d0cbcc93e9445a8a1a4f9
Vincent <hematusdvs@gmail.com>
2023-06-26 16:46:31 +0930

    dielectrics, camera placement

--- src/main.cpp
+++ src/main.cpp
@@ -34,25 +34,28 @@ colour ray_colour(const ray& r, const hittable& world, int depth) {
 int main() {
     // image
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 240;
+    const int image_width = 640;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
     const int max_depth = 50;
 
     // world
+
     hittable_list world;
 
     auto material_ground = make_shared<lambertian>(colour(0.8, 0.8, 0.0));
-    auto material_center = make_shared<dielectric>(1.5);
+    auto material_center = make_shared<lambertian>(colour(0.1, 0.2, 0.5));
     auto material_left = make_shared<dielectric>(1.5);
-    auto material_right = make_shared<metal>(colour(0.8, 0.6, 0.2), 1.0);
+    auto material_right = make_shared<metal>(colour(0.8, 0.6, 0.2), 0.0);
 
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_left));
     world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
-    // camera
-    camera cam;
+
+    // cam
+    camera cam(point3(-2, 2, 1), point3(0, 0, -1), vec3(0, 1, 0), 20, aspect_ratio);
 
     // Render
 
